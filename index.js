9f0a95cdb000e540c560fe13016edd6b265d3c12
Roman Gusev <rgusev@hwdtech.ru>
2018-12-14 12:57:19 +0600

    Add recommended config

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports = {
   rules: {
     "all-caps": require("./lib/rules/all-caps"),
     "single-colon": require("./lib/rules/single-colon")
+  },
+  configs: {
+    recommended: require("./lib/configs/recommended")
   }
 };
